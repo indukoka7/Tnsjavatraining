c7b1f1726b00f34281b9984dcace09023d347cdd
indukoka7 <indukoka7@gmail.com>
2026-01-04 20:54:29 +0530

    strings wrapper

---
+++ tnstraining/src/com/tnsif/Wrapper/Autoboxing.java
@@ -0,0 +1,19 @@
+package com.tnsif.Wrapper;
+
+public class Autoboxing {
+
+	public static void main(String[] args) {
+		int A=30;
+		Integer i=A;
+		System.out.println(i);
+		float B=23.4f;
+		float f =B;
+		System.out.println(f);
+	
+double C=345.34; 
+double d=C;
+   System.out.println(d);
+
+	}
+
+}
---
+++ tnstraining/src/com/tnsif/Wrapper/Autounboxing.java
@@ -0,0 +1,18 @@
+package com.tnsif.Wrapper;
+
+public class Autounboxing {
+
+	public static void main(String[] args) {
+		Integer i=50;
+		int x=i;
+		System.out.println(x);
+		float f=35.9f;
+		float y=f;
+		System.out.println(y);
+		double d=709.67f;
+		double z=d;
+		System.out.println(z);
+
+	}
+
+}
