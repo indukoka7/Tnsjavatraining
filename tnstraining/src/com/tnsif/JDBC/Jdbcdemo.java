53073da7b3f72d4caeb64b4f5c7ca94ce91e177b
indukoka7 <indukoka7@gmail.com>
2026-02-04 07:45:54 +0530

    jdbc

---
+++ tnstraining/src/com/tnsif/JDBC/Jdbcdemo.java
@@ -0,0 +1,36 @@
+package com.tnsif.JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Jdbcdemo {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		String url="jdbc:postgresql://localhost:5432/javaapplication";
+		String user="postgres";
+		String pwd="root";
+		//load driver
+		Class.forName("org.postgresql.Driver");
+		System.out.println("Driver loaded");
+		//Establish connection
+		Connection con = DriverManager.getConnection(url,user,pwd);
+		System.out.println("Connection Established");
+		//create a statement
+		Statement st = con.createStatement();
+		int i = st.executeUpdate("insert into student values(102,'Raj')");
+		System.out.println(i+" record updated successfully");
+		ResultSet rs = st.executeQuery("select * from student");
+		while(rs.next())
+		{
+			System.out.println("id = "+rs.getInt(1)+" sname= "+rs.getString(2));
+		}
+		st.close();
+		con.close();
+		
+
+	}
+
+}
